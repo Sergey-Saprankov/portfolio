56f41fd43bde75523f8f3182fe39367f82b76eb6
Sergey Saprankov <sergey-saprankov@mail.ru>
2023-01-17 22:37:22 +0300

    added contacts

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -1,64 +1,82 @@
-import React from 'react';
+import React from "react";
 import s from "./Contacts.module.scss";
 import Title from "../Title/Title";
+import location from "./img/location.svg";
+import telephone from "./img/telephone.svg";
+import email from "./img/email.svg";
+import telegram from "./img/telegram.svg";
 
 const Contacts = () => {
-    return (
-        <div className={s.container}>
-            <Title title={'Contacts'}/>
+  return (
+    <div className={s.container}>
+      <div className={s.animate}></div>
+      <Title title={"Contacts"} />
 
+      <div className={s.contentSection}>
+        <div className={s.infoSection}>
+          <div className={s.contactsBlock}>
+            <a className={s.link} href={"#"}>
+              <div className={s.imgContainer}>
+                <img className={s.img} src={location} alt="location" />
+              </div>
+              <div className={s.contactsBlockTitle}>Belarus, Zhlobin</div>
+            </a>
+          </div>
+          <div className={s.contactsBlock}>
+            <a className={s.link} href={"tel:+375293558603"}>
+              <div className={s.imgContainer}>
+                <img className={s.img} src={telephone} alt="telephone" />
+              </div>
 
-            <div className={s.contentSection}>
-                <div className={s.infoSection}>
-                    <div className={s.contactsBlock}>
-
-                    </div>
-                    <div className={s.contactsBlock}>
-
-                    </div>
-                    <div className={s.contactsBlock}>
-
-                    </div>
-                    <div className={s.contactsBlock}>
-
-                    </div>
-                </div>
-                <div className={s.formSection}>
-                    <div className={s.inputBlock}>
-                        <input className={s.input} type="text" placeholder={'Name'}/>
-                        <input className={s.input} type="email" placeholder={'Email'}/>
-                    </div>
-
-                    <textarea name="message" id="" cols="30" rows="10">
-
-                    </textarea>
-                </div>
-            </div>
-
+              <div className={s.contactsBlockTitle}>+375 ( 29 ) 355-86-03</div>
+            </a>
+          </div>
+          <div className={s.contactsBlock}>
+            <a className={s.link} href={"mailto:sergeysaprankov1992@gmail.com"}>
+              <div className={s.imgContainer}>
+                <img className={s.img} src={email} alt="email" />
+              </div>
+              <div className={s.contactsBlockTitle}>
+                sergeysaprankov1992@gmail.com
+              </div>
+            </a>
+          </div>
+          <div className={s.contactsBlock}>
+            <a
+              className={s.link}
+              target={"_blank"}
+              href={" https://www.t.me/sergeysaprankov"}
+            >
+              <div className={s.imgContainer}>
+                <img className={s.img} src={telegram} alt="telegram" />
+              </div>
+              <div className={s.contactsBlockTitle}>@sergeysaprankov</div>
+            </a>
+          </div>
         </div>
-    );
+
+        <form className={s.formSection}>
+          <div className={s.inputBlock}>
+            <input className={s.input} type="text" placeholder={"Name"} />
+            <input className={s.input} type="email" placeholder={"Email"} />
+          </div>
+
+          <textarea
+            className={s.textArea}
+            name="message"
+            id=""
+            cols="10"
+            rows="10"
+            placeholder={"Message"}
+          ></textarea>
+
+          <button className={s.submitBtn} type={"submit"}>
+            Send Message
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Contacts;
-
-
-// <div className={s.container}>
-//     <div className={s.animate}></div>
-//     <div className={s.wrapper}>
-//         <h2 className={s.title}>
-//             Contacts
-//         </h2>
-//         <div className={s.content}>
-//             <div className={s.description}>
-//                 <div>C</div>
-//                 <div>o</div>
-//                 <div>n</div>
-//                 <div>t</div>
-//                 <div>a</div>
-//                 <div>c</div>
-//                 <div>t</div>
-//                 <div>s</div>
-//             </div>
-//         </div>
-//     </div>
-// </div>
